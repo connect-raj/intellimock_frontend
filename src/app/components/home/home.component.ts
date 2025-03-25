27b7a1b16f439bb03d26cd34a289c8cead59abf5
Raj <connect.rajtrivedi@gmail.com>
2025-03-26 00:59:27 +0530

    feat: Added demo component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { HeaderComponent } from '../header/header.component';
 import { FeatureComponent } from './feature/feature.component';
+import { DemoAiComponent } from './demo-ai/demo-ai.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, HeaderComponent, FeatureComponent],
+  imports: [HeroComponent, HeaderComponent, FeatureComponent, DemoAiComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
